081f73ccc0139b9bbbb671a112e2680c578c4288
rian-hotate <kimakary9@gmail.com>
2020-07-10 09:13:43 +0900

    スレッド処理の追加

--- src/sample_opencv/src/main.rs
+++ src/sample_opencv/src/main.rs
@@ -1,6 +1,9 @@
 #![feature(conservative_impl_trait, universal_impl_trait)]
 extern crate glob;
 
+use std::sync::mpsc;
+use std::thread;
+
 use std::env::*;
 use std::path::PathBuf;
 use opencv::imgcodecs::*;
@@ -9,20 +12,44 @@ use opencv::objdetect::*;
 use opencv::*;
 use glob::glob;
 
-
-fn main() {
-    let ext = ".jpg";
-    let paths = input_filenames(ext).unwrap();
-    let imgs = load_img(paths).unwrap();
-    output_imgs(faceDetection(imgs, ext).unwrap(), ext);
+struct Worker {
+    img: Mat,
+    path: PathBuf,
 }
 
-fn load_img(paths: Vec<PathBuf>) -> Result<Vec<Mat>, ()> {
-    let mut v = Vec::new();
-    for path in paths {
-        v.push(imread(&path.into_os_string().into_string().unwrap(), IMREAD_UNCHANGED).unwrap());
+impl Worker {
+    fn new(img: Mat, path: PathBuf) -> Worker {
+        Worker {
+            img,
+            path,
+        }
     }
-    Ok(v)
+}
+
+fn main() {
+    let (tx_detection, rx_detection): (std::sync::mpsc::Sender<Worker>, std::sync::mpsc::Receiver<Worker>) = mpsc::channel();
+    let (tx, rx) =  mpsc::channel();
+
+    thread::spawn(move || {
+        println!("Suspending...");
+        for received in &rx_detection {
+            println!("Output Operating...");
+            let _ = output_img(Worker::new(face_detection(received.img, ".jpg").unwrap(), received.path), ".jpg");
+            let _ = tx.send(());
+        }
+    });
+
+    let ext = ".jpg";
+
+    for (path) in input_filenames(ext).unwrap().iter() {
+        let img = load_img(path.to_path_buf()).unwrap();
+        let _ = tx_detection.send(Worker::new(img, path.to_path_buf()));
+        let _ = rx.recv();   
+    }
+}
+
+fn load_img(path: PathBuf) -> Result<Mat, ()> {
+    Ok(imread(&path.into_os_string().into_string().unwrap(), IMREAD_UNCHANGED).unwrap())
 }
 
 pub fn input_filenames(ext: &str) -> Result<Vec<PathBuf>, std::io::Error> {
@@ -39,44 +66,31 @@ pub fn input_filenames(ext: &str) -> Result<Vec<PathBuf>, std::io::Error> {
     Ok(v)
 }
 
-pub fn output_filenames(ext: &str) -> Result<Vec<String>, std::io::Error> {
-    let paths = input_filenames(ext).unwrap();
-
-    let mut v = Vec::new();
-    for path in paths {
-        v.push(path.to_str().unwrap().replace("/input_images/", "/output_images/").replace(ext, &format!("{}{}", "_out", ext)));
-    }
-    Ok(v)
+pub fn output_filename(path: PathBuf, ext: &str) -> Result<String, ()> {
+    Ok(path.to_str().unwrap().replace("/input_images/", "/output_images/").replace(ext, &format!("{}{}", "_out", ext)))
 }
 
 
-fn output_imgs(imgs: Vec<Mat>, ext: &str) {
+fn output_img(work: Worker, ext: &str) {
     let mut v = Vector::new();
-    v.insert(0, IMWRITE_JPEG_CHROMA_QUALITY);
+    let _ = v.insert(0, IMWRITE_JPEG_CHROMA_QUALITY);
 
-    let paths = output_filenames(ext).unwrap();
-    for (index, img) in imgs.iter().enumerate() {
-        imwrite(&paths[index], &img, &v).unwrap();
-    }
+    let _ = imwrite(&output_filename(work.path, ext).unwrap(), &work.img, &v).unwrap();
 }
 
-fn faceDetection(imgs: Vec<Mat>, ext: &str) -> Result<Vec<Mat>, opencv::Error> {
+fn face_detection(img: Mat, ext: &str) -> Result<Mat, opencv::Error> {
 
     let mut face_cascade: CascadeClassifier = CascadeClassifier::new("/work/opencv/data/haarcascades/haarcascade_frontalface_default.xml").unwrap();
 
-    let mut v = Vec::new();
-    for img in imgs.iter() {
-        let mut src_img = Mat::copy(&img)?;
-        let mut gray = Mat::default()?;
-        imgproc::cvt_color(img, &mut gray, imgproc::COLOR_BGR2GRAY, 0);
-        let mut faces = types::VectorOfRect::new();
+    let mut src_img = Mat::copy(&img)?;
+    let mut gray = Mat::default()?;
+    let _ = imgproc::cvt_color(&img, &mut gray, imgproc::COLOR_BGR2GRAY, 0);
+    let mut faces = types::VectorOfRect::new();
 
-        face_cascade.detect_multi_scale(&gray, &mut faces, 1.3, 5, 0, Size_::new(150, 150), Size_::new(150, 150));
+    let _ = face_cascade.detect_multi_scale(&gray, &mut faces, 1.3, 5, 0, Size_::new(150, 150), Size_::new(150, 150));
 
-        for rect in faces.iter() {
-            imgproc::rectangle(&mut src_img, rect, Scalar_::new(255.0, 0.0, 0.0, 3.0), 1, 8, 0);
-        }
-        v.push(src_img);
+    for rect in faces.iter() {
+        let _ = imgproc::rectangle(&mut src_img, rect, Scalar_::new(255.0, 0.0, 0.0, 3.0), 1, 8, 0);
     }
-    Ok(v)
+    Ok(src_img)
 }
